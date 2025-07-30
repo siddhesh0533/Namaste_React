17441c97e7849f7dbe02f05375cedef902bcf8e8
Siddhesh <siddheshpatil0533@gmail.com>
2025-07-30 20:22:36 +0530

    episode 3 part 3

--- Ep_3/App.js
+++ Ep_3/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const Heading0 = () => <h1 className="boy">i'm a boy</h1>
+const Heading0 =()=> (<h1 className="boy">i'm a boy</h1>)
+const val = 1000;
 
 // const heading = React.createElement("h1", {id: "heading", sid:"my name"}, "HELLO WORLD USING REACT");
 // console.log(headind);
 const Heading = () => (<div>
     <h1>im a heading component</h1>
-    <Heading0 />
+    {val+1}
+    {20+90}
+    {Heading0()}
 </div>
 )
 
