4710fb986a5ec05d5c469ab6285229eed541e5b4
Siddhesh <siddheshpatil0533@gmail.com>
2025-08-06 02:40:02 +0530

    episode 6 part 2

--- Ep_6/src/components/Header.js
+++ Ep_6/src/components/Header.js
@@ -1,6 +1,10 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
+
+    const [buttoname, setButtoname] = useState("Login");
+
     return (
         <div className="header">
             <div className="logo">
@@ -12,6 +16,7 @@ const Header = () => {
                     <li>Contact US</li>
                     <li>About Us</li>
                     <li>Cart</li>
+                    <button className="btn" onClick={()=>{buttoname=== "Login"? setButtoname("Logout") : setButtoname("Login")}}>{buttoname}</button>
                 </ul>
             </div>
         </div>
