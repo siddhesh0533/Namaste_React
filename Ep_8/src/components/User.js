22d3b982da1f89c4624f1876f717ba520549fd95
Siddhesh <siddheshpatil0533@gmail.com>
2025-08-09 14:47:25 +0530

    episode 8 part 2

--- Ep_8/src/components/User.js
+++ Ep_8/src/components/User.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
 
-const User=({name, location})=>{
-    return(
+const User = ({ name, location }) => {
+    const [count] = useState(0);
+    const [count2] = useState(1);
+    return (
         <div>
+            <h3>Count: {count}</h3>
+            <h3>Count2: {count2}</h3>
             <h1>Name: {name}</h1>
             <h2>Location: {location}</h2>
             <h2>Contact: 1234567890</h2>
-            <hr/>
+            <hr />
         </div>
     )
 }
--- Ep_8/src/components/UserClass.js
+++ Ep_8/src/components/UserClass.js
@@ -2,18 +2,31 @@ import React from "react";
 
 class UserClass extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
+
+        this.state = {
+            count: 0,
+            count2: 1
+        }
     }
 
     render() {
-        const {name, location} = this.props;
+        const { name, location } = this.props;
+        const {count, count2} = this.state;
         return (
             <div>
+                <h3>Count: {count}</h3>
+                <button onClick={()=>{
+                    this.setState({
+                        count: this.state.count + 1,
+                    });
+                }}>count++</button>
+                <h3>Count2: {count2}</h3>
                 <h1>Name: {name}</h1>
                 <h2>Location: {location}</h2>
                 <h2>Contact: 1234567890</h2>
-                <hr/>
+                <hr />
             </div>
         );
     }
